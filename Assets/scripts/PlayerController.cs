71ea540f0a057c8133a3b4745797c05dba1cb7ae
1892809 <1892809@TE-GDA-D-D21E24>
2024-04-15 10:05:47 +0200

    1

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,71 +1,125 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float rotationSpeed = 100f;
     public float flySpeed = 5f;
+    //odniesienie do menadzera poziomu
+    GameObject levelManagerObject;
+    //stan osłon w procentach (1=100%)
+    float shieldCapacity = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        levelManagerObject = GameObject.Find("LevelManager");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //dodaj do wsp�rz�dnych warto�� x=1, y=0, z=0 pomno�one przez czas
+        //dodaj do współrzędnych wartość x=1, y=0, z=0 pomnożone przez czas
         //mierzony w sekundach od ostatniej klatki
         //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
 
-        //prezentacja dzia�ania wyg�adzonego sterowania (emualcja joystika)
+        //prezentacja działania wygładzonego sterowania (emualcja joystika)
         //Debug.Log(Input.GetAxis("Vertical"));
 
-        //sterowanie pr�dko�ci�
-        //stworz nowy wektor przesuni�cia o warto�ci 1 do przodu
+        //sterowanie prędkością
+        //stworz nowy wektor przesunięcia o wartości 1 do przodu
         Vector3 movement = transform.forward;
-        //pomn� go przez czas od ostatniej klatki
+        //pomnóż go przez czas od ostatniej klatki
         movement *= Time.deltaTime;
-        //pomn� go przez "wychylenie joystika"
+        //pomnóż go przez "wychylenie joystika"
         movement *= Input.GetAxis("Vertical");
-        //pomn� przez pr�dko�� lotu
+        //pomnóż przez prędkość lotu
         movement *= flySpeed;
         //dodaj ruch do obiektu
-        //smiana na fizyke
+        //zmiana na fizyke
         // --- transform.position += movement;
 
-        //komponent fizyki wewnatrz gracza
+        //komponent fizyki wewnątrz gracza
         Rigidbody rb = GetComponent<Rigidbody>();
+        //dodaj siłe - do przodu statku w trybie zmiany prędkości
+        rb.AddForce(movement, ForceMode.VelocityChange);
 
-        transform.GetComponent<Rigidbody>().AddForce(movement, ForceMode.VelocityChange);
 
-
-        //obr�t
-        //modyfikuj o� "Y" obiektu player
+        //obrót
+        //modyfikuj oś "Y" obiektu player
         Vector3 rotation = Vector3.up;
-        //przemn� przez czas
+        //przemnóż przez czas
         rotation *= Time.deltaTime;
-        //przemn� przez klawiatur�
+        //przemnóż przez klawiaturę
         rotation *= Input.GetAxis("Horizontal");
-        //pomn� przez pr�dko�� obrotu
+        //pomnóż przez prędkość obrotu
         rotation *= rotationSpeed;
-        //dodaj obr�t do obiektu
-        //e
-        //nie mozemy u�y� += poniewa� unity u�ywa Quaternion�w do zapisu rotacji
+        //dodaj obrót do obiektu
+        //nie możemy użyć += ponieważ unity używa Quaternionów do zapisu rotacji
         transform.Rotate(rotation);
-
+        UpdateUI();
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        //uruchamia się automatycznie jeśli zetkniemy się z innym coliderem
 
-        //sprawdz czy dotknelismy asteroidy
-        if(collision.collider.transform.CompareTag("Asteroid"))
+    private void UpdateUI()
+    {
+        //metoda wykonuje wszystko związane z aktualizacją interfejsu użytkownika
+
+        //wyciagnij z menadzera poziomu pozycje wyjscia
+        Vector3 target = levelManagerObject.GetComponent<LevelManager>().exitPosition;
+        //obroc znacznik w strone wyjscia
+        transform.Find("NavUI").Find("TargetMarker").LookAt(target);
+        //zmien ilosc procentwo widoczna w interfejsie
+        //TODO: poprawić wyświetlanie stanu osłon!
+        TextMeshProUGUI shieldText =
+            GameObject.Find("Canvas").transform.Find("ShieldCapacityText").GetComponent<TextMeshProUGUI>();
+        shieldText.text = " Shield: " + (shieldCapacity * 100).ToString() + "%";
+
+        //sprawdzamy czy poziom się zakończył i czy musimy wyświetlić ekran końcowy
+        if (levelManagerObject.GetComponent<LevelManager>().levelComplete)
         {
-            Debug.Log("Boom!");
-            //pauza
-            Time.timeScale = 0;
+            //znajdz canvas (interfejs), znajdz w nim ekran konca poziomu i go włącz
+            GameObject.Find("Canvas").transform.Find("LevelCompleteScreen").gameObject.SetActive(true);
+        }
+        //sprawdzamy czy poziom się zakończył i czy musimy wyświetlić ekran końcowy
+        if (levelManagerObject.GetComponent<LevelManager>().levelFailed)
+        {
+            //znajdz canvas (interfejs), znajdz w nim ekran konca poziomu i go włącz
+            GameObject.Find("Canvas").transform.Find("GameOverScreen").gameObject.SetActive(true);
         }
     }
-} 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //uruchamia się automatycznie jeśli zetkniemy sie z innym coliderem
+
+        //sprawdz czy dotknęliśmy asteroidy
+        if (collision.collider.transform.CompareTag("Asteroid"))
+        {
+            //transform asteroidy
+            Transform asteroid = collision.collider.transform;
+            //policz wektor według którego odepchniemy asteroide
+            Vector3 shieldForce = asteroid.position - transform.position;
+            //popchnij asteroide
+            asteroid.GetComponent<Rigidbody>().AddForce(shieldForce * 5, ForceMode.Impulse);
+            shieldCapacity -= 0.25f;
+            if (shieldCapacity <= 0)
+            {
+                //poinformuj level manager, że gra się skończyła bo nie mamy osłon
+                levelManagerObject.GetComponent<LevelManager>().OnFailure();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //jeżeli dotkniemy znacnzika końca poziomu to ustaw w levelmanager flagę,
+        //że poziom jest ukończony
+        if (other.transform.CompareTag("LevelExit"))
+        {
+            //wywołaj dla LevelManager metodę zakończenia poziomu
+            levelManagerObject.GetComponent<LevelManager>().OnSuccess();
+        }
+    }
+}
